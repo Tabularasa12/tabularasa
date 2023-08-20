7b8dc36c761ffe01e9b49fa9b5a3db3234174847
Tabularasa12 <etienne.mousseau.12@free.fr>
2023-08-21 00:13:11 +0200

    21/08/23 00:13

--- run.py
+++ run.py
@@ -16,11 +16,13 @@ if os.path.basename(args[0]) == os.path.basename(__file__):
         sys.argv = [path, "run"]
     elif len(args) > 1:
         if args[1] == 'clean':
+            path = init_path
             if len(args) == 3:
                 if args[2] != 'apps':
                     path = os.path.join(path, args[2])
                 else:
                     path = os.path.join(path, 'apps', args[2])
+            print(path)
             clean_py_path(path)
         if args[1] == 'debug':
             sys.argv = [path, "run", '--debug']
