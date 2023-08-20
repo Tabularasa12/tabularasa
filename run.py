f438b0a1dfeafbf26bb565f98740f509d14ad641
Tabularasa12 <etienne.mousseau.12@free.fr>
2023-08-20 21:53:47 +0200

    cleaning

--- run.py
+++ run.py
@@ -7,23 +7,24 @@ from flask.cli import main
 
 args = sys.argv
 file = sys.argv[0]
-path = os.path.dirname(__file__)
+init_path = os.path.dirname(__file__)
 
 if os.path.basename(args[0]) == os.path.basename(__file__):
-    os.chdir(path)
-    path = os.path.join(path, file)
+    os.chdir(init_path)
+    path = os.path.join(init_path, file)
     if len(args) == 1:
         sys.argv = [path, "run"]
     elif len(args) > 1:
         if args[1] == 'clean':
             if len(args) == 3:
                 if args[2] != 'apps':
-                    path = os.path.join(path, args[2])
+                    path = os.path.join(init_path, args[2])
                 else:
-                    path = os.path.join(path, 'apps', args[2])
-            clean_py_path(path)
+                    path = os.path.join(init_path, 'apps', args[2])
+            print(clean_py_path(init_path))
         if args[1] == 'debug':
-            sys.argv = [path, "run", '--debug']
+            sys.argv = [init_path, "run", '--debug']
     if 'run' in sys.argv:
         main()
+
 
