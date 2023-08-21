0af48a7e0d51ce36ed530ef9da85e812940d73c1
Tabularasa12 <etienne.mousseau.12@free.fr>
2023-08-21 19:43:06 +0200

    k

--- app.py
+++ app.py
@@ -40,39 +40,39 @@ def init_page():
 
 @default.route('/update/<string:mode>', methods=["POST"])
 def update(mode):
-    mode = True if mode == 'true' or mode == 'True' else False
-    log = {f"Tentative de mise à jour de {domain_name}" : time(DEFAULT_LOG_TIME_FORMAT)}
-    if mode:
-        host = 'www.pythonanywhere.com'
-        username = 'Tabularasa'
-        domain_name = 'tabularasa'
-        # request_host = host.replace('www', domain_name)
-        request_host = 'tabularasa.pythonanywhere.com'
-        token = '3f676d3102f7aada05843a6f0f04f4c49bb54a05'
-        log = {f"Début de mise à jour de {domain_name}" : time(DEFAULT_LOG_TIME_FORMAT)}
-        if request.host == request_host:
-            log[time(DEFAULT_LOG_TIME_FORMAT)] = f"Récupération des modifications sur le dépot Github de '{domain_name}'"
+    # mode = True if mode == 'true' or mode == 'True' else False
+    # log = {f"Tentative de mise à jour de {domain_name}" : time(DEFAULT_LOG_TIME_FORMAT)}
+    # if mode:
+    #     host = 'www.pythonanywhere.com'
+    #     username = 'Tabularasa'
+    #     domain_name = 'tabularasa'
+    #     # request_host = host.replace('www', domain_name)
+    #     request_host = 'tabularasa.pythonanywhere.com'
+    #     token = '3f676d3102f7aada05843a6f0f04f4c49bb54a05'
+    #     log = {f"Début de mise à jour de {domain_name}" : time(DEFAULT_LOG_TIME_FORMAT)}
+    #     if request.host == request_host:
+    #         log[time(DEFAULT_LOG_TIME_FORMAT)] = f"Récupération des modifications sur le dépot Github de '{domain_name}'"
             
-            response = subprocess.call(["git", "pull"])
+    #         response = subprocess.call(["git", "pull"])
             
-            if response.returncode == 200:
-                log[time(DEFAULT_LOG_TIME_FORMAT)] = f"{response.stdout}"
-            else:
-                log[time(DEFAULT_LOG_TIME_FORMAT)] = f"{response.stderr}"
+    #         if response.returncode == 200:
+    #             log[time(DEFAULT_LOG_TIME_FORMAT)] = f"{response.stdout}"
+    #         else:
+    #             log[time(DEFAULT_LOG_TIME_FORMAT)] = f"{response.stderr}"
         
-            import requests
-            response = requests.post(
-                f'https://{host}/api/v0/user/{username}/webapps/{domain_name}/reload/',
-                headers={'Authorization': f'Token {token}'}
-            )
+    #         import requests
+    #         response = requests.post(
+    #             f'https://{host}/api/v0/user/{username}/webapps/{domain_name}/reload/',
+    #             headers={'Authorization': f'Token {token}'}
+    #         )
             
-            if response.status_code == 200:
-                log[time(DEFAULT_LOG_TIME_FORMAT)] = f"L'application '{domain_name}' à bien été relancée"
-            else:
-                log[time(DEFAULT_LOG_TIME_FORMAT)] = f"Un problème est survenu lors du rechargement de l'application '{domain_name}'"
-    else:
-        log = {f"Mise à jour non activée" : time(DEFAULT_LOG_TIME_FORMAT)}
-    export_log = Json(f'./{DEFAULT_LOG_FILE}', host = request.host, update = log)
+    #         if response.status_code == 200:
+    #             log[time(DEFAULT_LOG_TIME_FORMAT)] = f"L'application '{domain_name}' à bien été relancée"
+    #         else:
+    #             log[time(DEFAULT_LOG_TIME_FORMAT)] = f"Un problème est survenu lors du rechargement de l'application '{domain_name}'"
+    # else:
+    #     log = {f"Mise à jour non activée" : time(DEFAULT_LOG_TIME_FORMAT)}
+    export_log = Json(f'./{DEFAULT_LOG_FILE}', host = request.host, update = mode)
     return dict()
 
 @default.route('/')
