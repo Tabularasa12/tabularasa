2796591d2252b4d88543fd0beb14201e7b7cfacc
Tabularasa12 <etienne.mousseau.12@free.fr>
2023-08-21 19:52:33 +0200

    n

--- app.py
+++ app.py
@@ -72,7 +72,7 @@ def update(mode):
     #             log[time(DEFAULT_LOG_TIME_FORMAT)] = f"Un problème est survenu lors du rechargement de l'application '{domain_name}'"
     # else:
     #     log = {f"Mise à jour non activée" : time(DEFAULT_LOG_TIME_FORMAT)}
-    export_log = Json(f'./{DEFAULT_LOG_FILE}', host = request.host, mode=mode)
+    export_log = Json(f'./{DEFAULT_LOG_FILE}', host = request.host, mode=False)
     return dict()
 
 @default.route('/')
