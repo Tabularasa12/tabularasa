4229726e6472d6a948e46ffdcc1b55545b6b4fee
Tabularasa12 <etienne.mousseau.12@free.fr>
2023-08-22 00:01:43 +0200

    k

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def update(mode):
     request_host = host.replace('www', domain_name)
     token = '3f676d3102f7aada05843a6f0f04f4c49bb54a05'
 
-    if not exist(json_path):
+    if not isfile(json_path):
         message = f"Début du log pour {domain_name}"
     json = Json(json_path, sort_key=False)
     if message:
