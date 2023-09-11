71f78339f97e1123ed560095dd863499834642b9
Tabularasa12 <etienne.mousseau.12@free.fr>
2023-09-11 15:37:00 +0200

    2# Please enter the commit message for your changes. Lines starting

--- utils/apps.py
+++ utils/apps.py
@@ -83,7 +83,7 @@ class Master(Flask):
             else:
                 return 'Wrong event type', 400
 
-        # essai1
+        # essai
         # import hashlib
         # import hmac
         # def verify_signature(payload_body, secret_token, signature_header):
