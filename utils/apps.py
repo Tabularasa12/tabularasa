5f35e10358fdd571496d456bcdaceb7cc606a182
Tabularasa12 <etienne.mousseau.12@free.fr>
2023-09-11 17:14:08 +0200

    g

--- utils/apps.py
+++ utils/apps.py
@@ -2,7 +2,6 @@ from functools import wraps
 from importlib import import_module
 import json
 import git
-print(git)
 
 from flask import (
     Blueprint,
@@ -76,36 +75,26 @@ class Master(Flask):
         @self.route('/update', methods=['POST'])
         def update():
             if request.method == 'POST':
-                print(request.headers.get('X-Hub-Signature'))
-                # print(request.data, request.files, request.headers)
-                # data = json.loads(request.data)
-                # print(data)
-                repo = git.Repo(join(self.root_path, '.git'))
-                origin = repo.remotes.origin
-                origin.pull()
-                return 'Updated PythonAnywhere successfully', 200
+                def verify_signature():
+                    from os import environ
+                    secret_key = environ.get('GIT_TOKEN')
+                    signature = "sha256="+hmac.new(bytes(secret_key, 'utf-8'), msg = request.data, digestmod = hashlib.sha256).hexdigest().lower()
+                    return hmac.compare_digest(signature, request.headers['X-Hub-Signature-256'])
+                if verify_signature():
+                    repo = git.Repo(join(self.root_path, '.git'))
+                    origin = repo.remotes.origin
+                    origin.pull()
+                    return 'Updated PythonAnywhere successfully', 200
+                else:
+                    raise HTTPException(status_code=403, detail="Request signatures didn't match!")
             else:
-                return 'Wrong event type', 400
+                raise HTTPException(status_code=400, detail="Wrong event type")
 
-        # essai
-        # import hashlib
-        # import hmac
-        # def verify_signature(payload_body, secret_token, signature_header):
-        #     """Verify that the payload was sent from GitHub by validating SHA256.
-
-        #     Raise and return 403 if not authorized.
-
-        #     Args:
-        #         payload_body: original request body to verify (request.body())
-        #         secret_token: GitHub app webhook token (WEBHOOK_SECRET)
-        #         signature_header: header received from GitHub (x-hub-signature-256)
-        #     """
-        #     if not signature_header:
-        #         raise HTTPException(status_code=403, detail="x-hub-signature-256 header is missing!")
-        #     hash_object = hmac.new(secret_token.encode('utf-8'), msg=payload_body, digestmod=hashlib.sha256)
-        #     expected_signature = "sha256=" + hash_object.hexdigest()
-        #     if not hmac.compare_digest(expected_signature, signature_header):
-        #         raise HTTPException(status_code=403, detail="Request signatures didn't match!")
+            def verify_signature(payload_body, secret_token, signature_header):
+                from os import environ
+                secret_key = environ.get('GIT_TOKEN')
+                signature = "sha256="+hmac.new(bytes(secret_key, 'utf-8'), msg = request.data, digestmod = hashlib.sha256).hexdigest().lower()
+                return hmac.compare_digest(signature, request.headers['X-Hub-Signature-256'])
 
         # @self.route('/update/<string:mode>', methods=["POST"])
         # def update(mode):
